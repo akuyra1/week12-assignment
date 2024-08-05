01f3c4b3c22ee4875093ba50d7ddf84cb92ecd04
Ramunas <akuyra1@gmail.com>
2024-08-05 12:05:01 +0100

    added more or less working sign up / sign in functionality.

---
+++ src/app/sign-up/[[...sign-up]]/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+//import the SignIn component
+import { SignUp } from '@clerk/nextjs'
+
+
+const SignUpPage = () => {
+  return (
+    <>
+      <div className='container'>
+          <h1>This is Sign Up page</h1>
+      </div>
+      <SignUp />
+    </>
+  )
+}
+
+export default SignUpPage
