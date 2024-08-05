90c0cbb1bd610681a2a13c5df56c3b55659038cd
orsella <emily.rever@outlook.com>
2024-08-05 14:30:08 +0100

    small fix, declared twice

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 import { Theme } from "@radix-ui/themes";
 import { ClerkProvider } from "@clerk/nextjs";
 
