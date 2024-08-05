de167835d0d2a1f8e5b2c6fb6d510dda6872c08b
Ramunas <akuyra1@gmail.com>
2024-08-05 09:24:26 +0100

    components folder added with Header.jsx

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import * as Popover from '@radix-ui/react-popover';
+import Link from 'next/link';
+import { Flex, Text, Button } from '@radix-ui/themes';
+
+
+
+export default function Header() {
+  return (
+    <>
+        <nav className='p-4 m-2s'>
+            <Link href={"/"} className='p-4 m-4 border-2'>Home</Link>
+            <Link href={"/"} className='p-4 m-2 border-2'>About</Link>
+            <Flex direction="column" gap="2">
+                <Text>Hello </Text>
+                <Button>Let's go</Button>
+            </Flex>
+        </nav>
+    </>
+  )
+}
+
+
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import '@radix-ui/themes/styles.css';
+
 
 export default function Home() {
   return (
