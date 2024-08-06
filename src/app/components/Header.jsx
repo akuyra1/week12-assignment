63596b3caed87cc5d7c2e95adcddd5e1b54733fd
Ramunas <akuyra1@gmail.com>
2024-08-06 16:53:23 +0100

    fixed burger

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -63,6 +63,7 @@ const Header = () => {
           <SignedIn>
             <UserButton afterSwitchSessionUrl="/" />
           </SignedIn>
+          <Hamburger />
         </div>
       </nav>
     </>
