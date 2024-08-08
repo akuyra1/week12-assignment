76bde5c1b0ce8831031a8454f079130760962d8c
Ramunas Kazlauskas <akuyra1@gmail.com>
2024-08-08 16:45:10 +0100

    some style issues fixed.

--- src/app/game/[slug]/page.js
+++ src/app/game/[slug]/page.js
@@ -34,17 +34,19 @@ export default async function Game({ params }) {
         />
       </SignedIn>
       <SignedOut>
-        <h1>To leave a comment, please sign up/sign in</h1>
-        <SignInButton
-          mode="modal"
-          className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-          forceRedirectUrl={`/game/${params.slug}`}
-        />
-        <SignUpButton
-          mode="modal"
-          className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-          forceRedirectUrl={`/game/${params.slug}`}
-        />
+        <div className="not-signed-in-container">
+          <h1>To leave a comment, please sign up/sign in</h1>
+          <SignInButton
+            mode="modal"
+            className="signInToCommentbtn text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            forceRedirectUrl={`/game/${params.slug}`}
+          />
+          <SignUpButton
+            mode="modal"
+            className="signUpToCommentBtn text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            forceRedirectUrl={`/game/${params.slug}`}
+          />
+        </div>
       </SignedOut>
       <GameComments params={params} />
     </div>
