42f374e5d9d4fbbc0da0473c1ba5c993e5908cc0
Joe Venton <joeventon@TechEd-0028.local>
2024-08-08 16:04:49 +0100

    arrrgh

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,29 +1,38 @@
-export default async function POST(req, res) {
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
+
   const { query } = req.body;
   const APIKEY = process.env.IGDB_API_KEY;
   const clientId = process.env.IGDB_CLIENT_ID;
 
-  const response = await fetch("https://api.igdb.com/v4/search", {
-    method: "POST",
-    headers: {
-      "Client-ID": clientId,
-      Authorization: `Bearer ${APIKEY}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      fields: "game.name,game.first_release_date,game.cover.*",
-      search: query,
-      limit: 10,
-      where: "game.version_parent = null",
-    }),
-  });
+  try {
+    const response = await fetch("https://api.igdb.com/v4/search", {
+      method: "POST",
+      headers: {
+        "Client-ID": clientId,
+        Authorization: `Bearer ${APIKEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        fields: "game.name,game.first_release_date,game.cover.*",
+        search: query,
+        limit: 10,
+        where: "game.version_parent = null",
+      }),
+    });
 
-  if (!response.ok) {
-    return res
-      .status(response.status)
-      .json({ error: "Network response was not ok" });
+    if (!response.ok) {
+      return res
+        .status(response.status)
+        .json({ error: "Network response was not ok" });
+    }
+
+    const data = await response.json();
+    return res.status(200).json(data);
+  } catch (error) {
+    console.error("Error fetching data from IGDB:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
-
-  const data = await request.json();
-  return new Response(JSON.stringify(data));
 }
