817713751e342ddeb02f0bb85765ca8aeff504b7
Joe Venton <joeventon@TechEd-0028.local>
2024-08-08 14:10:41 +0100

    more search stuff

--- src/app/search-result/page.js
+++ src/app/search-result/page.js
@@ -1,44 +1,16 @@
-import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
-import fetchSearch from "../utils/fetchSearch";
-import SearchBar from "../components/SearchBar";
+export default async function fetchSearch(query) {
+  const response = await fetch("/api/search", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ query }),
+  });
 
-export default function SearchResult() {
-  const router = useRouter();
-  const { query } = router.query;
+  if (!response.ok) {
+    throw new Error("Network response was bleeeh");
+  }
 
-  const [results, setResults] = useState([]);
-  const [searchQuery, setSearchQuery] = useState(query || "");
-
-  useEffect(() => {
-    if (searchQuery) {
-      fetchResults(searchQuery);
-    }
-  }, [searchQuery]);
-
-  const fetchResults = async (query) => {
-    try {
-      const data = await fetchSearch(query);
-      setResults(data);
-    } catch (error) {
-      console.error("Error fetching search results:", error);
-    }
-  };
-
-  const handleSearch = (query) => {
-    setSearchQuery(query);
-    router.push(`/search-result?query=${query}`);
-  };
-
-  return (
-    <div>
-      <h1>Search Results</h1>
-      <SearchBar onSearch={handleSearch} />
-      <ul>
-        {results.map((result, index) => (
-          <li key={index}>{result.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  const data = await response.json();
+  return data;
 }
